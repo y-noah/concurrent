3d533c5ea193a909103fa3e5335320b8127d937b
y-noah <hy.nhzj@gmail.com>
2024-04-16 08:51:39 +0800

    lim

--- main/java/com/example/concurrentserver/service/impl/TestServiceImpl.java
+++ main/java/com/example/concurrentserver/service/impl/TestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.concurrentserver.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.concurrentserver.entity.Age;
 import com.example.concurrentserver.entity.Test;
 import com.example.concurrentserver.entity.TestJoinAge;
@@ -9,6 +10,7 @@ import com.example.concurrentserver.mapper.TestMapper;
 import com.example.concurrentserver.service.ITestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,7 @@ import java.util.stream.Collectors;
 public class TestServiceImpl implements ITestService {
     @Autowired
     private TestMapper testMapper;
+    @Autowired
     private AgeMapper ageMapper;
 
     @Override
@@ -34,25 +37,56 @@ public class TestServiceImpl implements ITestService {
     public List<TestJoinAge> getTestJoinAge() {
         QueryWrapper<Test> testQueryWrapper = new QueryWrapper<>();
         QueryWrapper<Age> ageQueryWrapper = new QueryWrapper<>();
-
-        List<Test> tests = testMapper.selectList(testQueryWrapper);
+//        List<Test> tests = testMapper.selectList(testQueryWrapper);
         List<Age> ages = ageMapper.selectList(ageQueryWrapper);
+        Long count = testMapper.selectCount(testQueryWrapper);
+
+
+        int size = 50000;
+        double ceil = Math.ceil(count / size);
+        int offset = 0;
+        List<TestJoinAge> testJoinAges = new ArrayList<>();
+
+
+        for (int i = 1; i <= ceil; i++) {
+            Page<Test> page = new Page<>(offset, size);
+            Page<Test> p = testMapper.selectPage(page, null);
+            List<Test> records = p.getRecords();
+            offset = (offset+i) * size;
+
+            for (Test t : records) {
+                for (Age a : ages) {
+                    if (a.getMin() <= t.getAge() && a.getMax() >= t.getAge()) {
+                        TestJoinAge testJoinAge = new TestJoinAge();
+                        testJoinAge.setId(t.getId());
+                        testJoinAge.setName(t.getName());
+                        testJoinAge.setAge(t.getAge());
+                        testJoinAge.setText(t.getText());
+                        testJoinAge.setType(a.getType());
+
+                        testJoinAges.add(testJoinAge);
+                    }
+                }
+            }
+        }
+
+        return testJoinAges;
+
 
         // 试试分页查询分页处理，搭配多线程
-
-        return tests.parallelStream()
-                .flatMap(test -> ages.stream()
-                        .filter(age -> age.getMin() <= test.getAge() && test.getAge() <= age.getMax())
-                        .map(age -> {
-                            TestJoinAge testJoinAge = new TestJoinAge();
-                            testJoinAge.setId(test.getId());
-                            testJoinAge.setName(test.getName());
-                            testJoinAge.setAge(test.getAge());
-                            testJoinAge.setText(test.getText());
-                            testJoinAge.setType(age.getType());
-                            return testJoinAge;
-                        }))
-                .collect(Collectors.toList());
+//        return tests.parallelStream()
+//                .flatMap(test -> ages.stream()
+//                        .filter(age -> age.getMin() <= test.getAge() && test.getAge() <= age.getMax())
+//                        .map(age -> {
+//                            TestJoinAge testJoinAge = new TestJoinAge();
+//                            testJoinAge.setId(test.getId());
+//                            testJoinAge.setName(test.getName());
+//                            testJoinAge.setAge(test.getAge());
+//                            testJoinAge.setText(test.getText());
+//                            testJoinAge.setType(age.getType());
+//                            return testJoinAge;
+//                        }))
+//                .collect(Collectors.toList());
     }
 
 //    @Override
