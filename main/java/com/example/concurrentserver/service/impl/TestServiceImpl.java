951a92b93cdfc20965ec4e352cdf61f7f944208d
y-noah <hy.nhzj@gmail.com>
2024-05-06 18:50:05 +0800

    learn

--- main/java/com/example/concurrentserver/service/impl/TestServiceImpl.java
+++ main/java/com/example/concurrentserver/service/impl/TestServiceImpl.java
@@ -49,7 +49,7 @@ public class TestServiceImpl implements ITestService {
         double ceil = Math.ceil((double)count / size);
 //        int offset = 0;
 
-        List<TestJoinAge> testJoinAges = new ArrayList<>();
+//        List<TestJoinAge> testJoinAges = new ArrayList<>();
 //
 //        for (int i = 0; i < ceil; i++) {
 //            offset = i * size;
@@ -75,76 +75,76 @@ public class TestServiceImpl implements ITestService {
 //        }
 //        return testJoinAges;
 
-//        ExecutorService executorService = Executors.newFixedThreadPool(10); // 创建一个包含10个线程的固定大小线程池
-//
-//        // 使用CompletableFuture实现异步任务
-//        List<CompletableFuture<List<TestJoinAge>>> futures = new ArrayList<>();
-//
-//        for (int i = 0; i < ceil; i++) {
-//            int finalOffset =  i * size;
-//            CompletableFuture<List<TestJoinAge>> future = CompletableFuture.supplyAsync(() -> {
-//                System.out.println("finalOffset:" + finalOffset);
-//                Page<Test> page = new Page<>(finalOffset, size);
-//                Page<Test> p = testMapper.selectPage(page, null);
-//                List<Test> records = p.getRecords();
-//
-//                List<TestJoinAge> testJoinAges = new ArrayList<>();
-//                for (Test t : records) {
-//                    for (Age a : ages) {
-//                        if (a.getMin() <= t.getAge() && a.getMax() >= t.getAge()) {
-//                            TestJoinAge testJoinAge = new TestJoinAge();
-//                            testJoinAge.setId(t.getId());
-//                            testJoinAge.setName(t.getName());
-//                            testJoinAge.setAge(t.getAge());
-//                            testJoinAge.setText(t.getText());
-//                            testJoinAge.setType(a.getType());
-//
-//                            testJoinAges.add(testJoinAge);
-//                        }
-//                    }
-//                }
-//                return testJoinAges;
-//            }, executorService);
-//
-//            futures.add(future);
-//        }
-//
-//        // 等待所有任务完成，并收集结果
-//        List<TestJoinAge> testJoinAges = futures.stream()
-//                .flatMap(f -> f.join().stream())
-//                .collect(Collectors.toList());
-//
-//        executorService.shutdown(); // 关闭线程池
-//
-//        return testJoinAges;
+        ExecutorService executorService = Executors.newFixedThreadPool(10); // 创建一个包含10个线程的固定大小线程池
 
+        // 使用CompletableFuture实现异步任务
+        List<CompletableFuture<List<TestJoinAge>>> futures = new ArrayList<>();
 
-        // 并行流处理
-        IntStream.range(0, (int)ceil).parallel().forEach(i -> {
-            int offset = i * size;
-            System.out.println("offset:" + offset);
-            Page<Test> page = new Page<>(offset, size);
-            Page<Test> p = testMapper.selectPage(page, null);
-            List<Test> records = p.getRecords();
+        for (int i = 0; i < ceil; i++) {
+            int finalOffset =  i * size;
+            CompletableFuture<List<TestJoinAge>> future = CompletableFuture.supplyAsync(() -> {
+                System.out.println("finalOffset:" + finalOffset);
+                Page<Test> page = new Page<>(finalOffset, size);
+                Page<Test> p = testMapper.selectPage(page, null);
+                List<Test> records = p.getRecords();
 
-            records.forEach(t -> {
-                ages.forEach(a -> {
-                    if (a.getMin() <= t.getAge() && a.getMax() >= t.getAge()) {
-                        TestJoinAge testJoinAge = new TestJoinAge();
-                        testJoinAge.setId(t.getId());
-                        testJoinAge.setName(t.getName());
-                        testJoinAge.setAge(t.getAge());
-                        testJoinAge.setText(t.getText());
-                        testJoinAge.setType(a.getType());
+                List<TestJoinAge> testJoinAges = new ArrayList<>();
+                for (Test t : records) {
+                    for (Age a : ages) {
+                        if (a.getMin() <= t.getAge() && a.getMax() >= t.getAge()) {
+                            TestJoinAge testJoinAge = new TestJoinAge();
+                            testJoinAge.setId(t.getId());
+                            testJoinAge.setName(t.getName());
+                            testJoinAge.setAge(t.getAge());
+                            testJoinAge.setText(t.getText());
+                            testJoinAge.setType(a.getType());
 
-                        // 使用同步块确保线程安全地添加元素
-                        synchronized (testJoinAges) {
                             testJoinAges.add(testJoinAge);
                         }
                     }
-                });
-            });
-        });
+                }
+                return testJoinAges;
+            }, executorService);
+
+            futures.add(future);
+        }
+
+        // 等待所有任务完成，并收集结果
+        List<TestJoinAge> testJoinAges = futures.stream()
+                .flatMap(f -> f.join().stream())
+                .collect(Collectors.toList());
+
+        executorService.shutdown(); // 关闭线程池
+
+        return testJoinAges;
+
+
+        // 并行流处理
+//        IntStream.range(0, (int)ceil).parallel().forEach(i -> {
+//            int offset = i * size;
+//            System.out.println("offset:" + offset);
+//            Page<Test> page = new Page<>(offset, size);
+//            Page<Test> p = testMapper.selectPage(page, null);
+//            List<Test> records = p.getRecords();
+//
+//            records.forEach(t -> {
+//                ages.forEach(a -> {
+//                    if (a.getMin() <= t.getAge() && a.getMax() >= t.getAge()) {
+//                        TestJoinAge testJoinAge = new TestJoinAge();
+//                        testJoinAge.setId(t.getId());
+//                        testJoinAge.setName(t.getName());
+//                        testJoinAge.setAge(t.getAge());
+//                        testJoinAge.setText(t.getText());
+//                        testJoinAge.setType(a.getType());
+//
+//                        // 使用同步块确保线程安全地添加元素
+//                        synchronized (testJoinAges) {
+//                            testJoinAges.add(testJoinAge);
+//                        }
+//                    }
+//                });
+//            });
+//        });
 
         return testJoinAges;
 
